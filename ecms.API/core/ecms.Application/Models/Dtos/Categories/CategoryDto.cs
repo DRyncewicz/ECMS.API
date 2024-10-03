2ae52429b3eb1dd76461a0b371424b2c67ed05f7
KOMPUTER\patry <patrykbojarski96@gmail.com>
2024-10-04 00:22:22 +0200

    Feat: Endpoint and logic for retrieving categories
    -add getpagedcategories handler
    -add unit and integration tests
    -add mapper profiles

---
+++ ecms.API/core/ecms.Application/Models/Dtos/Categories/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ecms.Application.Models.Dtos.Categories;
+
+public class CategoryDto
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public HierarchyId HierarchyId { get; set; } = new();
+
+    public string AncestorName { get; set; } = string.Empty;
+}
