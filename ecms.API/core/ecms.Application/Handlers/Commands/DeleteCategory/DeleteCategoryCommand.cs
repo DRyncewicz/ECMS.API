e1c7f4db79468a6a44e8fd974e9e8309f26c260e
KOMPUTER\patry <patrykbojarski96@gmail.com>
2024-10-06 01:34:10 +0200

    Feat: Endpoint and logic for deleting category
    -add deletecategory handler
    -add unit and integration tests

---
+++ ecms.API/core/ecms.Application/Handlers/Commands/DeleteCategory/DeleteCategoryCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SharedKernel;
+
+namespace ecms.Application.Handlers.Commands.DeleteCategory;
+
+public class DeleteCategoryCommand : IRequest<Result<string>>
+{
+    public int CategoryId { get; set; }
+
+    public DeleteCategoryCommand(int categoryId)
+    {
+        CategoryId = categoryId;
+    }
+}
