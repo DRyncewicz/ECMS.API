15dd652ebcdb9318b493987036c8f84e9788eb31
Dominik Ryryry <ereence@gmail.com>
2024-10-26 02:19:10 +0200

    config: Change of identity provider from keycloak to duende

--- ecms.API/core/ecms.API/OpenApi/AddSwaggerGenWithAuth.cs
+++ ecms.API/core/ecms.API/OpenApi/AddSwaggerGenWithAuth.cs
@@ -1,47 +1,31 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 namespace ecms.API.OpenApi;
 
 internal static class ServiceCollectionExtensions
 {
-    internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services)
+    internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services, IConfiguration configuration)
     {
+        SwaggerOptions swaggerOptions = configuration.GetSection(SwaggerOptions.SectionName).Get<SwaggerOptions>()!;
+
         services.AddSwaggerGen(o =>
         {
-            o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
+            o.OperationFilter<AuthorizeCheckOperationFilter>();
+            o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme.ToLower(), new OpenApiSecurityScheme()
             {
                 Type = SecuritySchemeType.OAuth2,
-                Flows = new OpenApiOAuthFlows
+                Flows = new OpenApiOAuthFlows()
                 {
-                    Implicit = new OpenApiOAuthFlow
+                    AuthorizationCode = new OpenApiOAuthFlow
                     {
-                        AuthorizationUrl = new Uri("https://auth.ecms.ovh/realms/emcs/protocol/openid-connect/auth"),
-                        Scopes = new Dictionary<string, string>
-                         {
-                            { "openid", "openid" },
-                            { "profile", "profile" }
-                         }
+                        AuthorizationUrl = new Uri(swaggerOptions.AuthorizationUrl),
+                        TokenUrl = new Uri(swaggerOptions.TokenUrl),
+                        Scopes = swaggerOptions.Scopes,
                     }
                 }
             });
-
-            OpenApiSecurityScheme keycloakSecurityScheme = new()
-            {
-                Reference = new OpenApiReference
-                {
-                    Id = "Keycloak",
-                    Type = ReferenceType.SecurityScheme,
-                },
-                In = ParameterLocation.Header,
-                Name = "Bearer",
-                Scheme = "Bearer",
-            };
-
-            o.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                 { keycloakSecurityScheme, Array.Empty<string>() },
-             });
 
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
---
+++ ecms.API/core/ecms.API/OpenApi/SwaggerOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ecms.API.OpenApi;
+
+public class SwaggerOptions
+{
+    public string AuthorizationUrl { get; set; }
+    public string TokenUrl { get; set; }
+    public Dictionary<string, string> Scopes { get; set; }
+
+    public const string SectionName = "Swagger";
+}
