3b9f1e67e0ffed1e5bcb9f0a9d08c135db0926dc
KOMPUTER\patry <patrykbojarski96@gmail.com>
2024-11-30 20:35:08 +0100

    Feat: logic and endpoint to getSupplierDetailsById
    -add endpoint in SupplierControlloer
    -add query and handler
    -add supplierViewModel and supplierContactDto
    -add mappings in supplierProfile (entity to viewmodel and supplierContactEntity to supplierContactDTo)
    -add unit and integration tests
    -add mapping tests

---
+++ ecms.API/core/ecms.Application/Handlers/Queries/GetSupplierDetailsById/GetSupplierDetailsByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ecms.Application.Abstractions.Data;
+using ecms.Application.Models.ViewModels.Suppliers;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SharedKernal;
+using SharedKernel;
+
+namespace ecms.Application.Handlers.Queries.GetSupplierDetailsById;
+
+public class GetSupplierDetailsByIdQueryHandler(IApplicationDbContext _applicationDbContext,
+                                                IMapper _mapper) : IRequestHandler<GetSupplierDetailsByIdQuery, Result<SupplierDetailsViewModel>>
+{
+    public async Task<Result<SupplierDetailsViewModel>> Handle(GetSupplierDetailsByIdQuery request, CancellationToken ct)
+    {
+        var supplier = _applicationDbContext.Suppliers.Include(p => p.SupplierContacts).AsNoTracking().FirstOrDefault(p => p.Id == request.SupplierId);
+        Ensure.NotNull(supplier);
+        var model = _mapper.Map<SupplierDetailsViewModel>(supplier);
+        return Result.Success(model);
+    }
+}
--- ecms.API/tests/UnitTests/MapperProfiles/Suppliers/SupplierProfileTests.cs
+++ ecms.API/tests/UnitTests/MapperProfiles/Suppliers/SupplierProfileTests.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using ecms.Application.Handlers.Commands.CreateSupplier;
+using ecms.Application.Models.Dtos.Suppliers;
+using ecms.Application.Models.ViewModels.Suppliers;
 using ecms.Domain.Entities;
 using FluentAssertions;
 using UnitTests.Mapping;
@@ -55,5 +57,60 @@ public class SupplierProfileTests : IClassFixture<MappingTestFixture>
         result.Name.Should().Be(command.Name);
         result.SupplierId.Should().Be(command.Id);
         result.AddressId.Should().Be(command.AddressId);
+    }
+
+    [Fact]
+    public void Should_MapFrom_SupplierEntity_To_SupplierDetailsViewModel()
+    {
+        //Arrange
+        var command = new SupplierEntity()
+        {
+            Id = 1,
+            Name = "DodasekGrubasek",
+            AddressId = 1,
+            IsDeleted = false,
+            IsActive = true,
+        };
+
+        //Act
+        var result = _mapper.Map<SupplierDetailsViewModel>(command);
+
+        //Assert
+        result.IsActive.Should().Be(command.IsActive);
+        result.Id.Should().Be(command.Id);
+        result.Name.Should().Be(command.Name);
+        result.ContactDtos.Should().BeEmpty();
+        result.AddressId.Should().Be(command.AddressId);
+    }
+
+
+    [Fact]
+    public void Should_MapFrom_SupplierContactEntity_To_SupplierContactDto()
+    {
+        //Arrange
+        var command = new SupplierContactEntity()
+        {
+            Id = 1,
+            RepresentativeName = "Patrykos",
+            IsCommon = true,
+            SupplierId = 1,
+            PhoneNumber = "6954934543",
+            Email = "email@email.pl",
+            Description = "Description",
+            IsActive = true,
+        };
+
+        //Act
+        var result = _mapper.Map<SupplierContactDto>(command);
+
+        //Assert
+        result.Id.Should().Be(command.Id);
+        result.IsActive.Should().Be(command.IsActive);
+        result.IsCommon.Should().Be(command.IsCommon);
+        result.RepresentativeName.Should().Be(command.RepresentativeName);
+        result.SupplierId.Should().Be(command.SupplierId);
+        result.PhoneNumber.Should().Be(command.PhoneNumber);
+        result.Description.Should().Be(command.Description);
+        result.Email.Should().Be(command.Email);
     }
 }
